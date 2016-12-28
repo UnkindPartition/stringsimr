be35113bf5f4718b14c1b943aa8e02e4a33ef62b
Roman Cheplyaka <roma@ro-che.info>
2016-12-28 23:50:01 +0200

    Add min_len

--- R/stringsim.R
+++ R/stringsim.R
@@ -1,4 +1,4 @@
-stringsim <- function(a, b, match_value = 3, mismatch_value = -2, space_value = -5, part_value = -4) {
+stringsim <- function(a, b, match_value = 3, mismatch_value = -2, space_value = -5, part_value = -4, min_len = 0) {
   .Call("c_stringsim", utf8ToInt(a), utf8ToInt(b),
-        as.integer(c(match_value, mismatch_value, space_value, part_value)))
+        as.integer(c(match_value, mismatch_value, space_value, part_value, min_len)))
 }
--- src/stringsim.cpp
+++ src/stringsim.cpp
@@ -14,7 +14,7 @@ static vector<int> sexp2vec(SEXP sexp) {
 extern "C" {
   SEXP c_stringsim(SEXP a, SEXP b, SEXP scoring_vec) {
     const int *sv = INTEGER(scoring_vec);
-    const stringsim::Scoring scoring = { sv[0], sv[1], sv[2], sv[3] };
+    const stringsim::Scoring scoring = { sv[0], sv[1], sv[2], sv[3], sv[4] };
     SEXP result = allocVector(INTSXP, 1);
     INTEGER(result)[0] = stringsim::similarity(scoring, sexp2vec(a), sexp2vec(b));
     return result;
